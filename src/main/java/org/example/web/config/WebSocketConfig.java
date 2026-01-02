4df3776a5723c5e61b4aa5927ecbd62c2fc235ed
phandungnc <phanducdung2004@gmail.com>
2026-01-03 02:10:46 +0700

    fix conflict ws

--- src/main/java/org/example/web/config/WebSocketConfig.java
+++ src/main/java/org/example/web/config/WebSocketConfig.java
@@ -1,5 +1,8 @@
 package org.example.web.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.web.websocket.JwtHandshakeInterceptor;
+import org.example.web.websocket.UserHandshakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -8,18 +11,24 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+    private final UserHandshakeHandler userHandshakeHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-alert")
+                .addInterceptors(jwtHandshakeInterceptor)
+                .setHandshakeHandler(userHandshakeHandler)
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic", "/queue");
+        registry.enableSimpleBroker("/queue");
         registry.setUserDestinationPrefix("/user");
         registry.setApplicationDestinationPrefixes("/app");
     }
